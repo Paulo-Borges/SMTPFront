51ed06dc0f1348707a9345cf2426a14c5fcdb78f
Borges <borgespaulo72@yahoo.com.br>
2026-08-21 15:22:22 -0300

    feat: Configurando Github e Linkedin

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer implements OnInit, OnDestroy {
+  currentTime = signal(new Date());
+  internalId: any;
+
+  formatTime(zone:string) {
+    return this.currentTime().toLocaleTimeString('pt-BR', {
+      timeZone : zone,
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    })
+  }
+
+  formatDate(zone:string) {
+    return this.currentTime().toLocaleDateString('pt-BR', {
+      timeZone : zone,
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    })
+  }
+
+  ngOnInit(): void {
+    this.internalId = setInterval(() => {
+      this.currentTime.set(new Date());
+    }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.internalId)
+  }
+  
+}
